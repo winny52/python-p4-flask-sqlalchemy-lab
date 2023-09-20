3da2080bc47ea42584f2701c8d990058b6731118
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-20 21:59:14 +0300

    In progress

--- server/app.py
+++ server/app.py
@@ -19,15 +19,48 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {animal.id}<ul>
+        <ul>Name: {animal.name}<ul>
+        <ul>Species: {animal.species}<ul>
+        <ul>Zookeeper: {animal.zookeeper_id}<ul>
+        <ul>Enclosure: {animal.enclosure_id}<ul>
+    '''
+    response = make_response(response_body, 200)
+
+    return response
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {zookeeper.id}<ul>
+        <ul>Name: {zookeeper.name}<ul>
+        <ul>Birthday: {zookeeper.birthday}<ul>
+        <ul>Animal: {zookeeper.animals}<ul>
+    '''
+    response = make_response(response_body, 200)
+
+    return response
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {enclosure.id}<ul>
+        <ul>Environment: {enclosure.environment}<ul>
+        <ul>Open to visitors: {enclosure.open_to_visitors}<ul>
+        <ul>Animal: {enclosure.animals}<ul>
+    '''
+    response = make_response(response_body, 200)
+
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -5,19 +5,30 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
+# Assume 'app' is your Flask app instance
 db = SQLAlchemy(metadata=metadata)
 
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+    animals = db.relationship('Animal', backref='zookeeper')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    animals = db.relationship('Animal', backref='enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
